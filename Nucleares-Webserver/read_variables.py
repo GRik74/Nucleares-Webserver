6f1fe3ed111106e9fa7efe6462261d51fb221e0a
Gavan Rikard <rikardgavan2@gmail.com>
2025-09-07 23:49:35 -0400

    somdedits

--- Nucleares-Webserver/read_variables.py
+++ Nucleares-Webserver/read_variables.py
@@ -3,8 +3,8 @@ import requests
 portnum = 8080
 url = f"http://localhost:{portnum}/"
 
+
 def read_variable(variable):
     params = {"Variable": variable}
     response = requests.get(f"{url}", params=params)
     return response.text
-
